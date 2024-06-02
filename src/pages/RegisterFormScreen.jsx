5c0992bb9970103b21776be0d6129ea325c149be
Joshlion525 <78796082+Joshlion525@users.noreply.github.com>
2024-06-02 16:54:16 +0100

    adjusted the radio input to look like a button

--- src/pages/RegisterFormScreen.jsx
+++ src/pages/RegisterFormScreen.jsx
@@ -14,6 +14,7 @@ const RegisterFormScreen = () => {
 		phonenumber: "",
 		passport: "",
 	});
+
 	const handleChange = (event) => {
 		const { id, value } = event.target;
 		setFormData((prevData) => ({
@@ -21,12 +22,14 @@ const RegisterFormScreen = () => {
 			[id]: value,
 		}));
 	};
-	const handleRadioChange = (e) => {
+
+	const handleRadioChange = (value) => {
 		setFormData((prevData) => ({
 			...prevData,
-			passport: e.target.value,
+			passport: value,
 		}));
 	};
+
 	const submitData = (event) => {
 		event.preventDefault();
 		const dobDate = new Date(formData.dob);
@@ -43,12 +46,12 @@ const RegisterFormScreen = () => {
 			alert("You must be at least 18 years old.");
 			return;
 		}
-        if (formData.passport === "yes"){
-            navigate("/eligible")
-        }
-        if (formData.passport === "no"){
-            navigate("/noteligible")
-        }
+		if (formData.passport === "yes") {
+			navigate("/eligible");
+		}
+		if (formData.passport === "no") {
+			navigate("/noteligible");
+		}
 
 		console.log("Form Data Submitted:", formData);
 
@@ -62,6 +65,7 @@ const RegisterFormScreen = () => {
 			passport: "",
 		});
 	};
+
 	return (
 		<div className="backGround min-h-screen font-Ciutadella">
 			<Nav />
@@ -101,6 +105,7 @@ const RegisterFormScreen = () => {
 							type="date"
 							className="border border-none outline-none px-2 h-12 rounded-lg bg-[#b3b5b6] text-white "
 							id="dob"
+							max="2024-12-31"
 							value={formData.dob}
 							onChange={handleChange}
 							required
@@ -111,7 +116,7 @@ const RegisterFormScreen = () => {
 							Email Address
 						</label>
 						<input
-							type="mail"
+							type="email"
 							className="border border-none outline-none px-2 h-12 rounded-lg bg-[#b3b5b6] text-white "
 							id="email"
 							value={formData.email}
@@ -140,7 +145,7 @@ const RegisterFormScreen = () => {
 							Phone Number
 						</label>
 						<input
-							type="mail"
+							type="text"
 							className="border border-none outline-none px-2 h-12 rounded-lg bg-[#b3b5b6] text-white "
 							id="phonenumber"
 							value={formData.phonenumber}
@@ -149,40 +154,38 @@ const RegisterFormScreen = () => {
 						/>
 					</div>
 					<div className="flex flex-col gap-2 mb-5">
-						<label htmlFor="email" className="text-white">
+						<label htmlFor="passport" className="text-white">
 							Valid International Passport & USA Visa?
 						</label>
 						<div className="flex items-center justify-center gap-10 text-white">
-							<div className="flex items-center gap-2">
-								<input
-									type="radio"
-									id="yes"
-									name="passport"
-									value="yes"
-									checked={formData.passport === "yes"}
-									onChange={handleRadioChange}
-									required
-								/>
-								<label htmlFor="yes">Yes</label>
-							</div>
-							<div className="flex items-center gap-2">
-								<input
-									type="radio"
-									id="no"
-									name="passport"
-									value="no"
-									checked={formData.passport === "no"}
-									onChange={handleRadioChange}
-									required
-								/>
-								<label htmlFor="no">No</label>
-							</div>
+							<button
+								type="button"
+								className={`py-4 w-full rounded-lg ${
+									formData.passport === "yes"
+										? "bg-[#00833F]"
+										: "bg-gray-500"
+								} hover:bg-[#00833F]`}
+								onClick={() => handleRadioChange("yes")}
+							>
+								Yes
+							</button>
+							<button
+								type="button"
+								className={`py-4 w-full rounded-lg ${
+									formData.passport === "no"
+										? "bg-[#00833F]"
+										: "bg-gray-500"
+								} hover:bg-[#00833F]`}
+								onClick={() => handleRadioChange("no")}
+							>
+								No
+							</button>
 						</div>
 					</div>
 					<div className="flex justify-center mt-10">
 						<button
 							type="submit"
-							className="bg-[#00833F] text-white w-full py-3 px-20 rounded-sm"
+							className="bg-[#00833F] text-white w-full py-4 px-20 rounded-lg"
 						>
 							CREATE
 						</button>
